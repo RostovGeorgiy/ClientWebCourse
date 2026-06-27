141ec0237018ef3d5efabf3adaa716a3abbd9a83
RostovGeorgiy <perast201208@gmail.com>
2026-06-27 11:16:33 +0700

    Applied strict mode to entire file. Added arguments to functions. Changed array creation to be outside of function. Changed function and variable names.

--- WorkWithArrays/arrays.js
+++ WorkWithArrays/arrays.js
@@ -1,38 +1,39 @@
-(function() {
-    "use strict";
+"use strict";
 
+(function() {
     const array = [2, 5, 12, 11, 8, 43, 1, 0, -4, 3];
     console.log("Array created:");
     console.log(array);
 
-    function sortArray() {
+    function sortArrayDescending(array) {
         return array.sort((e1, e2) => e2 - e1);
     }
 
     console.log("Array sorted in descending order:");
-    console.log(sortArray());
+    console.log(sortArrayDescending(array));
 
-    function sliceArray(start, end) {
+    function sliceArray(array, start, end) {
         return array.slice(start, end);
     }
 
     console.log("Slices of an array. First 5 elements and last 5 elements:");
 
-    console.log(sliceArray(0, 5));
-    console.log(sliceArray(-5));
+    console.log(sliceArray(array, 0, 5));
+    console.log(sliceArray(array, -5));
 
-    function getArrayEvenNumbersSum() {
+    function getArrayEvenNumbersSum(array) {
         return array.reduce((sum, element) => element % 2 === 0 ? sum + element : sum, 0);
     }
 
     console.log("Sum of even numbers of an array:");
-    console.log(getArrayEvenNumbersSum());
+    console.log(getArrayEvenNumbersSum(array));
 
-    function getEvenArrayNumbersSquared() {
-        const newArray = Array.from({ length: 100 }, (_, index) => index + 1);
-        return newArray.filter(element => element % 2 === 0).map(element => element * element);
+    function getArrayEvenNumbersSquares(array) {
+        return array.filter(element => element % 2 === 0).map(element => element * element);
     }
 
     console.log("Squares of even numbers from 1-100 range:");
-    console.log(getEvenArrayNumbersSquared());
+
+    const hundredElementsArray = Array.from({ length: 100 }, (_, index) => index + 1);
+    console.log(getArrayEvenNumbersSquares(hundredElementsArray));
 })();
