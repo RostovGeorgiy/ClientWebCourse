3c5abaf1581e46dd2ca48e5cf327fa1dd321466b
RostovGeorgiy <perast201208@gmail.com>
2026-07-07 00:03:05 +0700

    Fixed formatting. Added async to self-calling function, removed throw in async/await, fixed "Download end" message not appearing.

--- Promise/promise.js
+++ Promise/promise.js
@@ -3,20 +3,20 @@
 (function() {
     function fetchJsonPromise(url) {
         console.log("Download start");
+
         fetch(url)
-        .then(response => {
-           if (!response.ok) {
-               throw new Error(`HTTP error! Status: ${response.status}`);
-           }
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
 
-           console.log("Download success");
-           return response.json();
-        })
-        .then(json => {
-            console.log(json);
-        })
-        .catch(e => console.log("Error:" + e));
-
+                return response.json();
+            })
+            .then(json => {
+                console.log(json);
+            })
+            .catch(e => console.log("Error: " + e))
+            .finally(() => console.log("Download end"));
     }
 
     fetchJsonPromise("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
--- Promise/promiseAsyncAwait.js
+++ Promise/promiseAsyncAwait.js
@@ -1,23 +1,20 @@
 "use strict";
 
-(function() {
+(async function() {
     async function fetchJsonAsync(url) {
         try {
             console.log("Download start");
-            let response = await fetch(url);
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
+            const response = await fetch(url);
+            const responseJson = await response.json();
 
-            console.log("Download success");
-
-            let responseJson = await response.json();
             console.log(responseJson);
         } catch (e) {
             console.log("Error: " + e);
+        } finally {
+            console.log("Download end");
         }
     }
 
-    fetchJsonAsync("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+    await fetchJsonAsync("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
 })();
